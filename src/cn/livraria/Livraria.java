41a209ed79f2b9a4abac061cca2e2ad179965d85
20232ADS.CNT0014 <carloswilliamwilliam7almeida@gmail.com>
2024-10-24 21:49:34 -0300

    Adicionando livraria

---
+++ src/cn/livraria/Livraria.java
@@ -0,0 +1,37 @@
+package cn.livraria;
+
+public class Livraria {
+    int capacidade = 100;
+
+    Livro livros[];
+    int size;
+    Livraria(int tam){
+        livros = new Livro[tam];
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public void adicionar(Livro livro) throws  RuntimeException{
+        if (size == livros.length){
+           throw new RuntimeException("Não dá mais!!");
+        }
+        livros[size]  = livro;
+        size++;
+    }
+
+    public Livro buscarLivro(String nome) throws  RuntimeException{
+        for (int i = 0; i < livros.length; i++){
+            Livro livro= livros[i];
+            if (livro.nome.equals(nome)){
+                return livro;
+            }
+        }
+        throw new RuntimeException("Livro não encontrado");
+    }
+}
